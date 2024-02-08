f1db0e4f6ac8f2ad3f2b564c5866542bdee65842
Carlos Palhares <chjunior@gmail.com>
2024-02-08 15:37:09 -0300

    Move PerconaAdapter::Column spec to a separate file

--- spec/active_record/connection_adapters/percona_adapter_spec.rb
+++ spec/active_record/connection_adapters/percona_adapter_spec.rb
@@ -1,48 +1,6 @@
 require 'spec_helper'
 
 describe ActiveRecord::ConnectionAdapters::DepartureAdapter do
-  describe ActiveRecord::ConnectionAdapters::DepartureAdapter::Column do
-    let(:field) { double(:field) }
-    let(:default) { double(:default) }
-    let(:cast_type) do
-      if defined?(ActiveRecord::ConnectionAdapters::AbstractMysqlAdapter::MysqlString)
-        ActiveRecord::ConnectionAdapters::AbstractMysqlAdapter::MysqlString.new
-      else
-        ActiveRecord::Type.lookup(:string, adapter: :mysql2)
-      end
-    end
-    let(:metadata) do
-      ActiveRecord::ConnectionAdapters::SqlTypeMetadata.new(
-        type: cast_type.type,
-        sql_type: type,
-        limit: cast_type.limit
-      )
-    end
-    let(:mysql_metadata) do
-      ActiveRecord::ConnectionAdapters::MySQL::TypeMetadata.new(metadata)
-    end
-    let(:type) { 'VARCHAR' }
-    let(:null) { double(:null) }
-    let(:collation) { double(:collation) }
-
-    let(:column) do
-      if ActiveRecord::VERSION::STRING >= "6.1"
-        described_class.new("field", "default", mysql_metadata, null, collation: "collation")
-      else
-        described_class.new(field, default, mysql_metadata, null, collation: collation)
-      end
-    end
-
-    describe '#adapter' do
-      subject { column.adapter }
-      it do
-        is_expected.to eq(
-          ActiveRecord::ConnectionAdapters::DepartureAdapter
-        )
-      end
-    end
-  end
-
   let(:mysql_adapter) do
     instance_double(ActiveRecord::ConnectionAdapters::Mysql2Adapter)
   end

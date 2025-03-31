beb6ac7ef7fb77605380fbe95296b5ce9b246f05
Austin Story <lonnieastory@gmail.com>
2025-03-31 09:18:17 -0700

    Update version compatability to use Gem::Requirement for detecting version compatability

--- spec/support/version_compatibility.rb
+++ spec/support/version_compatibility.rb
@@ -2,17 +2,7 @@ module VersionCompatibility
   def self.compatible?(version_string, compatibility_string)
     raise "Invalid Gem Version: '#{version_string}'" unless Gem::Version.correct?(version_string)
 
-    comparator, target = compatibility_string.match(/(\D*?)\s*(\d+.*)/).captures
-    raise "Incorrect Gem Version for target: '#{target}'" unless Gem::Version.correct? target
-
-    case comparator
-    when '<'  then version_string < target
-    when '<=' then version_string <= target
-    when '>'  then version_string > target
-    when '>=' then version_string >= target
-    else
-      raise "Unsupported Compatability string: '#{activerecord_compatibility}'\
-      , don't know how to compare using '#{comparator}'"
-    end
+    requirement = Gem::Requirement.new(compatibility_string)
+    requirement.satisfied_by?(Gem::Version.new(version_string))
   end
 end

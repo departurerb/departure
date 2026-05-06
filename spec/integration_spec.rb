deba9d9387a5e69857d7df226891e92295a3c842
Douglas Soares de Andrade <contato@douglasandrade.com>
2026-05-06 10:31:14 -0400

    Fix trilogy integration adapter expectation

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -59,9 +59,11 @@ describe Departure, integration: true do
     let(:db_config) { Configuration.new }
 
     it 'uses PerconaAdapter while preserving the application connection' do
+      departure_adapter = ENV.fetch('DB_ADAPTER', 'mysql2') == 'trilogy' ? 'percona_trilogy' : 'percona'
+
       expect(Departure::ConnectionBase)
         .to receive(:establish_connection)
-        .with(hash_including(adapter: 'percona'))
+        .with(hash_including(adapter: departure_adapter))
         .and_call_original
 
       run_a_migration(direction, 1)
